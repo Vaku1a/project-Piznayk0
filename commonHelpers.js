d800ddd47565fb491dd3ea4374ef408e72f7fc6c
Vaku1a <110764648+Vaku1a@users.noreply.github.com>
2023-11-09 14:45:55 +0000

    Deploying to gh-pages from @ Vaku1a/project-Piznayk0@e3885ad396b7f1b94753ce8e9e907190ebd7c685 🚀

--- commonHelpers.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";console.log("Hello Home-page");
-//# sourceMappingURL=commonHelpers.js.map
--- commonHelpers2.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";console.log("Hello Shipping Page");
-//# sourceMappingURL=commonHelpers2.js.map
