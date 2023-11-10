a20360e2175b31c8f1c450a8b288c8285c27b11a
Roman-Lviv <kituraroman@gmail.com>
2023-11-10 19:30:01 +0200

    git add .

--- src/js/partials/support.js
+++ src/js/partials/support.js
@@ -2,25 +2,37 @@ const supportList = document.querySelector('.support-list');
 const supportListItems = document.querySelectorAll('.support-list .li-support');
 const scrollButton = document.getElementById('scrollButton');
 
+let currentIndex = 0;
+
+// Функція для оновлення виділеного елемента та прокрутки до нього
+function updateHighlightedItem() {
+  supportListItems.forEach(i => i.classList.remove('highlighted'));
+  supportListItems[currentIndex].classList.add('highlighted');
+
+  supportListItems[currentIndex].scrollIntoView({
+    behavior: 'smooth',
+  });
+}
+
+// Додаємо слухача подій для кнопки прокрутки вниз
+scrollButton.addEventListener('click', () => {
+  if (currentIndex < supportListItems.length - 1) {
+    currentIndex++;
+    updateHighlightedItem();
+  } else {
+    // Якщо досягли кінця списку, прокручуємо вгору
+    currentIndex = 0;
+    supportListItems[currentIndex].scrollIntoView({
+      behavior: 'smooth',
+      block: 'start', // Прокручувати вгору до верхнього краю блоку
+    });
+  }
+});
+
 // Додаємо слухача подій для кожного елемента списку
 supportListItems.forEach((item, index) => {
   item.addEventListener('click', () => {
-    // Видаляємо підсвічування з усіх елементів
-    supportListItems.forEach(i => i.classList.remove('highlighted'));
-
-    // Додаємо підсвічування тільки клікнутому елементу
-    item.classList.add('highlighted');
+    currentIndex = index;
+    updateHighlightedItem();
   });
-});
-
-// Додаємо функціонал для кнопки прокрутки вниз
-scrollButton.addEventListener('click', () => {
-  const highlightedIndex = Array.from(supportListItems).findIndex(item =>
-    item.classList.contains('highlighted')
-  );
-  if (highlightedIndex < supportListItems.length - 1) {
-    supportListItems[highlightedIndex + 1].scrollIntoView({
-      behavior: 'smooth',
-    });
-  }
 });
