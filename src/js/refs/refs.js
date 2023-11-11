5c1abe98ac94710ce17c0d833f07c85f729806e7
KorolenkoDaria <admem75213@gmail.com>
2023-11-11 22:25:04 +0200

    created header

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -7,7 +7,10 @@ export const refs = {
   body: document.querySelector('body'),
 
   //header
-
+  mobileMenu: document.querySelector('.js-menu-container'),
+  openMenuBtn: document.querySelector('.js-open-menu'),
+  closeMenuBtn: document.querySelector('.js-close-menu'),
+  toggleEl: document.querySelector('.header-toggle-bg'),
   //categories
 
   //support
