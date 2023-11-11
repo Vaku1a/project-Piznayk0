a8df4483e28939277ed1b81309f881142b784985
Vaku1a <110764648+Vaku1a@users.noreply.github.com>
2023-11-11 17:00:38 +0200

    localStorege (partial) + html + css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './js/api/api';
+import './js/utils/localstorage/localStorageApi';
 import './js/partials/header';
 import './js/partials/categories';
 import './js/partials/support';
 import './js/partials/booklist';
 import './js/partials/book-card-popup';
+
 // import './js/partials/sign-in';
