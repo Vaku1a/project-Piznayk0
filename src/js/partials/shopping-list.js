50c97c8ffd3879715fe79abb7ce30e8582d5f48f
Kouk02 <kouk20020703@gmail.com>
2023-11-11 16:56:46 +0200

    add(delete-btn)

--- src/js/partials/shopping-list.js
+++ src/js/partials/shopping-list.js
@@ -1,69 +1,137 @@
-// Список портів
-import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
-import jsonData from '../partials/data.json';
 
-// Шукаємо клас shoping-list
-const shoppingList = document.querySelector('.shopping-list');
 
-// Кількість карточок на сторінці
-const cardsPerPage = 4;
 
-// Розділення даних на сторінки
-const paginatedData = [];
-for (let i = 0; i < jsonData.length; i += cardsPerPage) {
-  paginatedData.push(jsonData.slice(i, i + cardsPerPage));
+
+const shoppingList = document.querySelector('.shopping-list');//шукаємо клас shoping-list
+shoppingList.addEventListener('click', onClick);// Додаємо обробник подій до списку
+
+
+function onClick(event) {
+  if (event.target.classList.contains("delete-book")) { //шукає кнопку на якій відбулась подія
+    const listItem = event.target.closest(".shopping-item"); // далі на кнопці на якій відбулась подія знаходить елемент з класом shopping-item
+    if (listItem) {
+      const itemId = listItem.id; // далі отримуємо ID елемент тієї li 
+      shoppingList.removeChild(listItem); // видаляємо елемент із shoppingList
+      removeFromLocalStorage(itemId); // видаляємо збережений елемент з localStorage
+    }
+  }
 }
 
-// SVG-код для видалення
-const deleteIconSVG = `
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-  <path d="M6 2H10M2 4H14M12.6667 4L12.1991 11.0129C12.129 12.065 12.0939 12.5911 11.8667 12.99C11.6666 13.3412 11.3648 13.6235 11.0011 13.7998C10.588 14 10.0607 14 9.00623 14H6.99377C5.93927 14 5.41202 14 4.99889 13.7998C4.63517 13.6235 4.33339 13.3412 4.13332 12.99C3.90607 12.5911 3.871 12.065 3.80086 11.0129L3.33333 4M6.66667 7V10.3333M9.33333 7V10.3333" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-`;
+//! Отримати дані з localStorage
+const fromlocalstr = JSON.parse(localStorage.getItem('shoppingItems')) || [];
 
-// Створюємо розмітку (картку) для кожної книжки
-const pagesHTML = paginatedData.map(pageData => {
-  return pageData.map(book => `
-    <li class="shopping-item">
-      <div class="shopping-card">
-        <div class="shopping-image">
-          <img src="${book.book_image}" alt="${book.title}" class="shopping-image">
-        </div>
-        <div class="shopping-info">
-          <h2 class="book-title">${book.title}</h2>
-          <p class="book-category">${book.publisher}</p>
-          <p class="book-desc">${book.description}</p>
-          <p class="book-author">${book.author}</p>
-        </div>
-        <button class="delete-book">${deleteIconSVG}</button>
-        <div class="extra-logo">
-          <img class="amazon" src="./img/amazon.svg" alt="amazon" />
-          <img class "apple" src="./img/apple.svg" alt="apple" />
+//!функція на видалення по id картки з розмітки і local storage
+  function removeFromLocalStorage(id) {
+  const items = JSON.parse(localStorage.getItem('shoppingItems')) || fromlocalstr;// зчитуємо дані з loc.str під ключем, якщо даних немає то отримуємо пустий масив
+  const updatedItems = items.filter(item => item._id !== id);// шукаєм елементи які потрібно видалити і створюємо новий масив
+  localStorage.setItem('shoppingItems', JSON.stringify(updatedItems)); // оновлюємо дані в localStorage вже з видаленими об'єктами
+}
+
+
+
+
+if (fromlocalstr.length === 0) { // Якщо немає даних в localStorage, створюємо розмітку для пустої сторінки
+  
+  const emptyPageHTML = `
+      <div class="shopping-wrap-none">
+        <div class="container-none-card">
+          <h1 class="shopping-title-none">
+            Shopping <span class="shopping-title-span-none">List</span>
+          </h1>
+          <p class="shopping-text-none">
+            This page is empty, add some books and proceed to order.
+          </p>
+          <img
+            class="image-big"
+            src="../img/shopping-list/empty-bin@2x.png"
+            alt="empty list"
+          />
+          <img
+            class="image-small"
+            src="../img/shopping-list/empty-bin@1x.png"
+            alt="empty list"
+          />
         </div>
       </div>
-    </li>
-  `).join('');
-});
+  `;
 
-// Створюємо пагінацію
-const pagination = new Pagination('pagination', {
-  totalItems: paginatedData.length,
-  itemsPerPage: 1, // Один елемент на сторінку оскільки ми маємо HTML для кожної сторінки
-});
-
-// Функція, яка відображає відповідну сторінку
-const updatePage = (pageIndex) => {
-  shoppingList.innerHTML = pagesHTML[pageIndex - 1];
-  
-};
-
-// Встановлюємо обробник подій для зміни сторінки пагінації
-pagination.on('afterMove', (event) => {
-  updatePage(event.page);
-});
+  shoppingList.innerHTML = emptyPageHTML;
+} else {
 
 
-// Відображаємо першу сторінку при завантаженні
-updatePage(1);
+//!Створюємо розмітку (картку) якщо у нас є об'єкти в local storage
+const bookCardsHTML = fromlocalstr.map(book => ` 
+  <li class="shopping-item" id ="${book._id}">
+    <div class="shopping-card">
+      <div class="shopping-image">
+        <img src="${book.book_image}" alt="${book.title}" class="shopping-image">
+      </div>
+      <div class="shopping-info">
+        <h2 class="book-title">${book.title}</h2>
+        <p class="book-category">${book.publisher}</p>
+        <p class="book-desc">${book.description}</p>
+        <p class="book-author">${book.author}</p>
+      </div>
+      <button class="delete-book">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+        >
+          <path
+            d="M6 2H10M2 4H14M12.6667 4L12.1991 11.0129C12.129 12.065 12.0939 12.5911 11.8667 12.99C11.6666 13.3412 11.3648 13.6235 11.0011 13.7998C10.588 14 10.0607 14 9.00623 14H6.99377C5.93927 14 5.41202 14 4.99889 13.7998C4.63517 13.6235 4.33339 13.3412 4.13332 12.99C3.90607 12.5911 3.871 12.065 3.80086 11.0129L3.33333 4M6.66667 7V10.3333M9.33333 7V10.3333"
+            stroke="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+      <div class="extra-logo">
+        <img class="amazon" src="./img/amazon.svg" alt="amazon" />
+        <img class="apple" src="./img/apple.svg" alt="apple" />
+      </div>
+    </div>
+  </li>
+`).join('');
+
+
+//! Добавляє створений html батьківський елемент в .shopping-list
+shoppingList.innerHTML = bookCardsHTML;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
