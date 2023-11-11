b60560a2a8834606824b4e9c2589ee7b2b00984d
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2023-11-11 20:11:32 +0200

    support section completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './js/api/api';
 import './js/partials/header';
 import './js/partials/categories';
-import './js/partials/support';
+import './js/partials/charity';
 import './js/partials/booklist';
 import './js/partials/book-card-popup';
 // import './js/partials/sign-in';
