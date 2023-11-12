398528efc168d6d050bbb5ceb6936ffe0d25e6f0
Vaku1a <110764648+Vaku1a@users.noreply.github.com>
2023-11-12 18:30:40 +0200

    add scroll js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import './js/partials/categories';
 import './js/partials/charity';
 import './js/partials/booklist';
 import './js/partials/book-card-popup';
-
+import './js/partials/scroll-up';
 // import './js/partials/sign-in';
