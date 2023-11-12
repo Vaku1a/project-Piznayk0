2f80d6ac2c4eaa0945365aa9942a98e6f366b297
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-11-12 19:12:20 +0200

    all js mostly

--- src/js/partials/book-card-popup.js
+++ src/js/partials/book-card-popup.js
@@ -1,9 +1,11 @@
 import { getBooksId } from '../api/api.js';
-console.log('hELLO wORLD!');
+import { refs } from '../refs/refs';
+
 const popupBookCardEl = document.querySelector('.popup-create-markup');
 const popupBtnAddRemoveEl = document.querySelector('.popup-btn-add-remove');
 const popupAddMessageEl = document.querySelector('.popup-add-message');
 const STORAGE_KEY = 'bookList';
+
 let dataFromStorage = getDataFromStorage(STORAGE_KEY)
   .then(data => {
     return data ? data : [];
@@ -12,40 +14,47 @@ let dataFromStorage = getDataFromStorage(STORAGE_KEY)
     console.log(error.message);
   });
 let newBook = {};
-
 popupAddMessageEl.hidden = true;
+refs.booksCaregoriesContainer.addEventListener('click', callPopupWindow);
+popupBookCardEl.previousElementSibling.addEventListener('click', () =>
+  popupBookCardEl.parentNode.parentNode.classList.toggle('is-hidden')
+);
 popupBtnAddRemoveEl.addEventListener('click', addBookToStorage);
 popupBtnAddRemoveEl.addEventListener('click', removeBookFromStorage);
+
+function callPopupWindow(evt) {
+  evt.preventDefault();
+  popupBookCardEl.parentNode.parentNode.classList.toggle('is-hidden');
+  getBooksId(evt.target.dataset.bookId).then(data => {
+    newBook = data;
+    checkingBookList(newBook);
+    let markup = createMarkup(newBook);
+    addBookMarkup(markup);
+  });
+}
 
 async function getDataFromStorage(key) {
   const response = await localStorage.getItem(key);
   const data = await JSON.parse(response);
   return data;
 }
-// getDataFromStorage(STORAGE_KEY).then(a => {
-//   console.log(a);
-//   return data ? JSON.parse(data) : [];
-// }).catch ((error) => {
-//   console.log(error.message);
-//  })
-
-// function getDataFromStorage(key) {
-//   try {
-//     let data = localStorage.getItem(STORAGE_KEY);
-//     return data ? JSON.parse(data) : [];
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-function checkingBookList(data) {
-  let arr = [];
-  dataFromStorage.then(res => {
-    arr = res;
-  });
+async function addDataToStorage(key, value) {
   try {
-    const bookId = data._id;
-    let status = true;
-    status = arr.some(({ _id }) => _id === bookId);
+    await localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+async function checkingBookList(data) {
+  try {
+    const arr = await getDataFromStorage(STORAGE_KEY)
+      .then(data => {
+        return data ? data : [];
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+    const status = arr.some(({ _id }) => _id === data._id);
     if (status) {
       popupBtnAddRemoveEl.removeEventListener('click', addBookToStorage);
       popupBtnAddRemoveEl.textContent = 'remove from the shopping list';
@@ -58,11 +67,9 @@ function checkingBookList(data) {
     console.log(error.message);
   }
 }
-
 function addBookMarkup(markup) {
   popupBookCardEl.innerHTML = markup;
 }
-
 function createMarkup({
   book_image,
   title,
@@ -86,28 +93,33 @@ function createMarkup({
 
   return markup;
 }
-
-function addBookToStorage() {
-  let arr = [];
-  dataFromStorage.then(res => {
-    arr = res;
-  });
-  console.log(arr);
-  arr.push(newBook);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(arr));
-  location.reload();
+async function addBookToStorage() {
+  try {
+    const dataFromStorage = await getDataFromStorage(STORAGE_KEY)
+      .then(data => {
+        return data ? data : [];
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+    dataFromStorage.push(newBook);
+    addDataToStorage(STORAGE_KEY, dataFromStorage);
+    location.reload();
+  } catch (error) {
+    console.log(error.message);
+  }
 }
-function removeBookFromStorage() {
-  const clearStorage = dataFromStorage.filter(book => {
-    book._id !== newBook._id;
-  });
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(clearStorage));
-  location.reload();
-}
+async function removeBookFromStorage() {
+  try {
+    const dataFromStorage = await getDataFromStorage(STORAGE_KEY);
 
-getBooksId('643282b1e85766588626a0dc').then(data => {
-  newBook = data;
-  checkingBookList(newBook);
-  let markup = createMarkup(newBook);
-  addBookMarkup(markup);
-});
+    const clearStorage = dataFromStorage.filter(
+      book => book._id !== newBook._id
+    );
+    addDataToStorage(STORAGE_KEY, clearStorage);
+
+    location.reload();
+  } catch (error) {
+    console.log(error.message);
+  }
+}
