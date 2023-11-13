9d27c4a8ba6c90abcbfbb83cc2050f3b95cc2909
Vaku1a <110764648+Vaku1a@users.noreply.github.com>
2023-11-13 22:01:37 +0000

    Deploying to gh-pages from @ Vaku1a/project-Piznayk0@0e6f1bcd9f95c4dd670d497a3995f0d54bd06c5c 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -13,7 +13,7 @@ import{g as h,r as a,a as y,b as E,c as x}from"./assets/sign-in-0b001ff7.js";imp
             </h2>
             <div class="book-categories-container">
             ${f(o)}
-            </div>`)}).catch(o=>{console.error(o)});a.booksPart.addEventListener("click",B);function B(o){if(o.target.nodeName!=="BUTTON")return;window.scrollTo(0,0);const s=o.target.parentElement.querySelector(".book-category-title").textContent;y(s).then(t=>{if(!t||t.length===0){a.booksPart.innerHTML=`${c(s)}
+            </div>`)}).catch(o=>{console.error(o)});a.booksPart.addEventListener("click",B);function B(o){if(o.target.nodeName!=="BUTTON")return;window.scrollTo(0,0);const s=o.target.parentElement.querySelector(".book-category-title").textContent;y(s).then(e=>{if(!e||e.length===0){a.booksPart.innerHTML=`${c(s)}
                 <div class="book-category-wrapper">
                 <div class="books-not-found-wrapper">
                 <p class="books-not-found-message">No books were found in this category😒<br> Please, try other categories😉</p>
@@ -29,15 +29,15 @@ import{g as h,r as a,a as y,b as E,c as x}from"./assets/sign-in-0b001ff7.js";imp
                 </div>`;return}a.booksPart.innerHTML=`${c(s)}
             <div class="book-category-wrapper">
                 <ul class="book-cards-list book-cards-list-one-category">
-                ${p(t)}                  
+                ${p(e)}                  
                 </ul>                             
-            </div>`,a.booksPart.querySelectorAll(".book-cards-list-item").forEach(n=>n.style.display="block")}).catch(t=>{console.error(t)})}function f(o){return o.map(({list_name:e,books:s})=>`<div class="book-category-container">
-                <h3 class="book-category-title">${e}</h3>
+            </div>`,a.booksPart.querySelectorAll(".book-cards-list-item").forEach(n=>n.style.display="block")}).catch(e=>{console.error(e)})}function f(o){return o.map(({list_name:t,books:s})=>`<div class="book-category-container">
+                <h3 class="book-category-title">${t}</h3>
                 <ul class="book-cards-list">
                 ${p(s)}                  
                 </ul>
                 <button type="button" class="see-more-btn">See more</button>             
-            </div>`).join("")}E().then(o=>{a.categoriesList.innerHTML=L(o)}).catch(o=>{console.error(o)});a.categoriesList.addEventListener("click",S);function S(o){if(o.target.nodeName!=="LI")return;window.scrollTo(0,0);const e=o.target;e.parentElement.querySelector(".categories-list-item.active").classList.remove("active"),e.classList.add("active");const s=o.target.dataset.categoryName;if(s==="All categories"){h().then(t=>{if(!t||t.length===0){a.booksPart.innerHTML=`<h2 class="books-part-title">Best Sellers
+            </div>`).join("")}E().then(o=>{a.categoriesList.innerHTML=S(o)}).catch(o=>{console.error(o)});a.categoriesList.addEventListener("click",L);function L(o){if(o.target.nodeName!=="LI")return;window.scrollTo(0,0);const t=o.target;t.parentElement.querySelector(".categories-list-item.active").classList.remove("active"),t.classList.add("active");const s=o.target.dataset.categoryName;if(s==="All categories"){h().then(e=>{if(!e||e.length===0){a.booksPart.innerHTML=`<h2 class="books-part-title">Best Sellers
                         <span class="books-part-title-span"> Books</span>
                         </h2>
                         <div class="book-categories-container">
@@ -56,8 +56,8 @@ import{g as h,r as a,a as y,b as E,c as x}from"./assets/sign-in-0b001ff7.js";imp
                     <span class="books-part-title-span"> Books</span>
                     </h2>
                     <div class="book-categories-container">
-                    ${f(t)}
-                    </div>`}).catch(t=>{console.error(t)});return}y(s).then(t=>{if(!t||t.length===0){a.booksPart.innerHTML=`${c(s)}
+                    ${f(e)}
+                    </div>`}).catch(e=>{console.error(e)});return}y(s).then(e=>{if(!e||e.length===0){a.booksPart.innerHTML=`${c(s)}
                     <div class="book-category-wrapper">
                     <div class="books-not-found-wrapper">
                     <p class="books-not-found-message">No books were found in this category😒<br> Please, try other categories😉</p>
@@ -73,9 +73,9 @@ import{g as h,r as a,a as y,b as E,c as x}from"./assets/sign-in-0b001ff7.js";imp
                     </div>`;return}a.booksPart.innerHTML=`${c(s)}
             <div class="book-category-wrapper">
                 <ul class="book-cards-list book-cards-list-one-category">
-                ${p(t)}                  
+                ${p(e)}                  
                 </ul>                             
-            </div>`,a.booksPart.querySelectorAll(".book-cards-list-item").forEach(n=>n.style.display="block")}).catch(t=>{console.error(t)})}function L(o){const e=o.map(({list_name:s})=>`<li class="categories-list-item" data-category-name="${s}">${s}</li>`).sort((s,t)=>s.localeCompare(t));return e.unshift('<li class="categories-list-item active" data-category-name="All categories">All categories</li>'),e.join("")}function c(o){const e=o.split(" ");if(e.includes("and")){const n=e.indexOf("and"),i=e.slice(0,n+1).join(" "),k=e.slice(n+1).join(" ");return`<h2 class="books-part-title">${i} <span class="books-part-title-span">${k}</span></h2>`}if(e.length>3){const n=e.slice(0,e.length/2).join(" "),i=e.slice(e.length/2).join(" ");return`<h2 class="books-part-title">${n} <span class="books-part-title-span">${i}</span></h2>`}const s=e.pop();return`<h2 class="books-part-title">${e.join(" ")} <span class="books-part-title-span">${s}</span></h2>`}function p(o){return o.map(({_id:e,book_image:s,author:t,title:n})=>`<li class="book-cards-list-item" data-book-id="${e}">
+            </div>`,a.booksPart.querySelectorAll(".book-cards-list-item").forEach(n=>n.style.display="block")}).catch(e=>{console.error(e)})}function S(o){const t=o.map(({list_name:s})=>`<li class="categories-list-item" data-category-name="${s}">${s}</li>`).sort((s,e)=>s.localeCompare(e));return t.unshift('<li class="categories-list-item active" data-category-name="All categories">All categories</li>'),t.join("")}function c(o){const t=o.split(" ");if(t.includes("and")){const n=t.indexOf("and"),i=t.slice(0,n+1).join(" "),k=t.slice(n+1).join(" ");return`<h2 class="books-part-title">${i} <span class="books-part-title-span">${k}</span></h2>`}if(t.length>3){const n=t.slice(0,t.length/2).join(" "),i=t.slice(t.length/2).join(" ");return`<h2 class="books-part-title">${n} <span class="books-part-title-span">${i}</span></h2>`}const s=t.pop();return`<h2 class="books-part-title">${t.join(" ")} <span class="books-part-title-span">${s}</span></h2>`}function p(o){return o.map(({_id:t,book_image:s,author:e,title:n})=>`<li class="book-cards-list-item" data-book-id="${t}">
             <img
             class="book-card-img"
             src="${s}"
@@ -86,11 +86,11 @@ import{g as h,r as a,a as y,b as E,c as x}from"./assets/sign-in-0b001ff7.js";imp
                 <p>quick view</p>
             </div>
             <p class="book-card-title">${n}</p>
-            <p class="book-card-author">${t}</p>
-        </li>`).join("")}const l="bookList";let r={};a.booksPart.addEventListener("click",w);a.popupBookCardEl.previousElementSibling.addEventListener("click",d);a.popupBookCardEl.parentNode.parentNode.addEventListener("click",d);document.addEventListener("keydown",d);function d(o){(o.target===o.currentTarget||o.key==="Escape")&&(a.popupBookCardEl.parentNode.parentNode.classList.toggle("is-hidden"),a.body.classList.toggle("modal-open"))}function w(o){o.preventDefault(),a.popupBookCardEl.parentNode.parentNode.classList.toggle("is-hidden"),a.body.classList.toggle("modal-open"),x(o.target.dataset.bookId).then(e=>{r=e,m(r);let s=C(r);T(s)})}async function g(o){const e=await localStorage.getItem(o);return await JSON.parse(e)}async function v(o,e){try{await localStorage.setItem(o,JSON.stringify(e))}catch(s){console.log(s.message)}}async function m(o){try{(await g(l).then(t=>t||[]).catch(t=>{console.log(t.message)})).some(({_id:t})=>t===o._id)?(a.popupBookCardEl.nextElementSibling.removeEventListener("click",u),a.popupBookCardEl.nextElementSibling.addEventListener("click",b),a.popupBookCardEl.nextElementSibling.textContent="remove from the shopping list",a.popupBookCardEl.nextElementSibling.nextElementSibling.hidden=!1):(a.popupBookCardEl.nextElementSibling.removeEventListener("click",b),a.popupBookCardEl.nextElementSibling.addEventListener("click",u),a.popupBookCardEl.nextElementSibling.textContent="Add to shopping list",a.popupBookCardEl.nextElementSibling.nextElementSibling.hidden=!0)}catch(e){console.log(e.message)}}function T(o){a.popupBookCardEl.innerHTML=o}function C({book_image:o,title:e,author:s,description:t,buy_links:[n,i]}){return`<img src="${o}" class="popup-image" />
-            <h2 class="popup-book-title">${e}</h2>
+            <p class="book-card-author">${e}</p>
+        </li>`).join("")}const l="bookList";let r={};a.booksPart.addEventListener("click",w);a.popupBookCardEl.previousElementSibling.addEventListener("click",d);a.popupBookCardEl.parentNode.parentNode.addEventListener("click",d);document.addEventListener("keydown",d);function d(o){(o.target===o.currentTarget||o.key==="Escape")&&(a.popupBookCardEl.parentNode.parentNode.classList.toggle("is-hidden"),a.body.classList.toggle("popup-modal-open"))}function w(o){if(o.target===o.currentTarget)return;o.preventDefault(),a.popupBookCardEl.parentNode.parentNode.classList.toggle("is-hidden"),a.body.classList.toggle("popup-modal-open");const t=o.target.closest("LI").dataset.bookId||o.target.parentNode.dataset.bookId;x(t).then(s=>{r=s,m(r);let e=C(r);T(e)})}async function g(o){const t=await localStorage.getItem(o);return await JSON.parse(t)}async function v(o,t){try{await localStorage.setItem(o,JSON.stringify(t))}catch(s){console.log(s.message)}}async function m(o){try{(await g(l).then(e=>e||[]).catch(e=>{console.log(e.message)})).some(({_id:e})=>e===o._id)?(a.popupBookCardEl.nextElementSibling.removeEventListener("click",u),a.popupBookCardEl.nextElementSibling.addEventListener("click",b),a.popupBookCardEl.nextElementSibling.textContent="remove from the shopping list",a.popupBookCardEl.nextElementSibling.nextElementSibling.hidden=!1):(a.popupBookCardEl.nextElementSibling.removeEventListener("click",b),a.popupBookCardEl.nextElementSibling.addEventListener("click",u),a.popupBookCardEl.nextElementSibling.textContent="Add to shopping list",a.popupBookCardEl.nextElementSibling.nextElementSibling.hidden=!0)}catch(t){console.log(t.message)}}function T(o){a.popupBookCardEl.innerHTML=o}function C({book_image:o,title:t,author:s,description:e,buy_links:[n,i]}){return`<img src="${o}" class="popup-image" />
+            <h2 class="popup-book-title">${t}</h2>
             <p class="popup-book-author">${s}</p>
-            <p class="popup-book-description">${t}</p>
+            <p class="popup-book-description">${e}</p>
             <div class="popup-links">
                 <a href="${n.url}" target="_blank"><img class="popup-link-img" src="./img/modal-shop/amazon@1x.png"
                         srcset="./img/modal-shop/amazon@1x.png, ./img/modal-shop/amazon@2x.png"
@@ -99,5 +99,5 @@ import{g as h,r as a,a as y,b as E,c as x}from"./assets/sign-in-0b001ff7.js";imp
                         srcset="./img/modal-shop/apple-books@1x.png, ./img/modal-shop/apple-books@2x.png"
                         alt="link to apple books" /></a>
             </div>
-            `}async function u(){try{const o=await g(l).then(e=>e||[]).catch(e=>{console.log(e.message)});o.push(r),v(l,o),m(r)}catch(o){console.log(o.message)}}async function b(){try{const e=(await g(l)).filter(s=>s._id!==r._id);v(l,e),m(r)}catch(o){console.log(o.message)}}window.onscroll=()=>{$()};function $(){document.body.scrollTop>20||document.documentElement.scrollTop>20?document.getElementById("back-to-up").classList.remove("is-hidden"):document.getElementById("back-to-up").classList.add("is-hidden")}(()=>{function o(){window.scrollTo({top:0,behavior:"smooth"})}document.querySelector("#back-to-up").addEventListener("click",o)})();
+            `}async function u(){try{const o=await g(l).then(t=>t||[]).catch(t=>{console.log(t.message)});o.push(r),v(l,o),m(r)}catch(o){console.log(o.message)}}async function b(){try{const t=(await g(l)).filter(s=>s._id!==r._id);v(l,t),m(r)}catch(o){console.log(o.message)}}window.onscroll=()=>{$()};function $(){document.body.scrollTop>20||document.documentElement.scrollTop>20?document.getElementById("back-to-up").classList.remove("is-hidden"):document.getElementById("back-to-up").classList.add("is-hidden")}(()=>{function o(){window.scrollTo({top:0,behavior:"smooth"})}document.querySelector("#back-to-up").addEventListener("click",o)})();
 //# sourceMappingURL=commonHelpers.js.map
