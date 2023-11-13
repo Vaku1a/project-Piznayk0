2e445f9f027564f0bc60fb63212e60721e338dfb
Kouk02 <kouk20020703@gmail.com>
2023-11-13 10:52:08 +0200

    final-commit

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,33 +1,35 @@
-import { refs } from '../refs/refs';    
+// import { refs } from '../refs/refs';
 
-/* MOBILE MENU */
+// /* MOBILE MENU */
 
-refs.openMenuBtn.addEventListener('click', toggleOpenModal);
-refs.closeMenuBtn.addEventListener('click', toggleCloseModal);
+// refs.openMenuBtn.addEventListener('click', toggleOpenModal);
+// refs.closeMenuBtn.addEventListener('click', toggleCloseModal);
 
-function toggleOpenModal() {
-  if (refs.mobileMenu.classList.contains('is-hidden')) {
-    refs.mobileMenu.classList.remove('is-hidden');
-      refs.closeMenuBtn.classList.remove('is-hidden');
-      refs.openMenuBtn.classList.add('is-hidden')
-    return;
-  } 
-}
-function toggleCloseModal() {
-    refs.mobileMenu.classList.add('is-hidden');
-    refs.closeMenuBtn.classList.add('is-hidden');
-    refs.openMenuBtn.classList.remove('is-hidden')
-}
+// function toggleOpenModal() {
+//   if (refs.mobileMenu.classList.contains('is-hidden')) {
+//     refs.mobileMenu.classList.remove('is-hidden');
+//     refs.closeMenuBtn.classList.remove('is-hidden');
+//     refs.openMenuBtn.classList.add('is-hidden');
+//     return;
+//   }
+// }
+// function toggleCloseModal() {
+//   refs.mobileMenu.classList.add('is-hidden');
+//   refs.closeMenuBtn.classList.add('is-hidden');
+//   refs.openMenuBtn.classList.remove('is-hidden');
+// }
 
-/* TOGGLE THEME */
+// /* TOGGLE THEME */
 
-refs.toggleEl.addEventListener('click', toggleClass);
+// refs.toggleEl.addEventListener('click', toggleClass);
 
-function toggleClass(evt) {    
-  refs.body.classList.toggle('body-dark-theme');
-  localStorage.setItem('darkTheme', refs.body.classList.contains('body-dark-theme'))
-}
-if (localStorage.getItem('darkTheme') === 'true') {
-  refs.body.classList.add('body-dark-theme')
-}
-
+// function toggleClass(evt) {
+//   refs.body.classList.toggle('body-dark-theme');
+//   localStorage.setItem(
+//     'darkTheme',
+//     refs.body.classList.contains('body-dark-theme')
+//   );
+// }
+// if (localStorage.getItem('darkTheme') === 'true') {
+//   refs.body.classList.add('body-dark-theme');
+// }
